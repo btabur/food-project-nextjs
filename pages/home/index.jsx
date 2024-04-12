00f16017064b79e21c384fd0e39d29997ff1a1cc
Balı Tabur <webtasarim.0323@gmail.com>
2024-04-12 09:58:21 +0300

    project files created

---
+++ pages/home/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Index = () => {
+  return (
+    <div>
+      
+      <h1 className='text-red-700 font-dancing'>home page</h1>
+    </div>
+  )
+}
+
+export default Index
