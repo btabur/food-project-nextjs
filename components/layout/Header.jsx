d9a4c5e2035b490138ddec11183173c12e2d513f
Balı Tabur <webtasarim.0323@gmail.com>
2024-06-18 11:15:27 +0300

    frontend/made header menu responsive

--- components/layout/Header.jsx
+++ components/layout/Header.jsx
@@ -4,16 +4,23 @@ import { FaUserAlt } from "react-icons/fa";
 import { FaShoppingCart } from "react-icons/fa";
 import { FaSearch } from "react-icons/fa";
 import Seach from "../ui/Seach";
+import { IoMenu } from "react-icons/io5";
+import { IoCloseCircleOutline } from "react-icons/io5";
+
 
 
 const Header = () => {
-  const [isSearchModal,setIsSearchModal] = useState(false)
+  const [isSearchModal,setIsSearchModal] = useState(false);
+  const [isShowMenu,setIsShowMenu]=useState(false);
   return (
     <header className="h-[5.5rem] bg-secondary ">
       <div className="container mx-auto flex justify-between items-center text-white h-full">
         <Logo />
-        <nav>
-          <ul className="flex gap-10">
+        <nav className={`sm:static absolute top-0 right-0 
+        grid place-content-center h-full w-full sm:text-white text-black sm:w-auto sm:h-auto 
+          sm:bg-transparent bg-white 
+          ${!isShowMenu && "hidden"} `}>
+          <ul className="flex gap-10 sm:flex-row flex-col">
             <li className=" px-[5px] py-[10px] hover:text-primary cursor-pointer">
               HOME
             </li>
@@ -27,6 +34,10 @@ const Header = () => {
               BOOK TABLE
             </li>
           </ul>
+          {isShowMenu && <button onClick={()=> setIsShowMenu(false)} className="absolute top-3 right-3 z-[55]">
+                <IoCloseCircleOutline className=" text-3xl hover:text-primary transition-all" />
+              </button>}
+        
         </nav>
         <div className="flex gap-4 items-center justify-center">
           <a href="#">
@@ -38,9 +49,13 @@ const Header = () => {
           <button onClick={()=>setIsSearchModal(true)}>
           <FaSearch className="hover:text-primary transition-all" />
           </button>
-         <a href="">
+         <a href="" className="md:inline-block hidden">
          <button className="btn-primary">Order Online</button>
          </a>
+         <button onClick={()=>setIsShowMenu(true)} className="sm:hidden inline-block">
+         <IoMenu className="hover:text-primary transition-all"  />
+         </button>
+       
         </div>
       </div>
       {isSearchModal && 
